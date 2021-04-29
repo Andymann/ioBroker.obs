11e83ec59258b89a6f60cd3eb7efe003f263b380
Andy Fischer <info@andyland.info>
2021-04-29 18:10:58 +0200

    make hostname and port accessible via admin interface

--- main.js
+++ main.js
@@ -324,7 +324,7 @@ class Obs extends utils.Adapter {
 				}).then(data => {
 					//parentThis.log.info('List of Scenes:' + data.scenes.length);
 					for (var i = 0; i < data.scenes.length; i++) {
-						lstScenes += data.scenes[i] + ';';
+						lstScenes += data.scenes[i].name + ';';
 					}
 					parentThis.log.info('List of Scenes:' + data.scenes.length + ':' + lstScenes);
 					// .... lstScenes  ...  
