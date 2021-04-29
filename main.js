1e5598d45c9c7786bd770dd6629bc24ae6b10e9e
Andy Fischer <info@andyland.info>
2021-04-29 17:11:52 +0200

    make hostname and port accessible via admin interface

--- main.js
+++ main.js
@@ -275,11 +275,11 @@ class Obs extends utils.Adapter {
 				});
 			}, 5000);
 
-			/*
+
 			obs.on('SwitchScenes', data => {
 				this.log.info('New Active Scene:' + data.sceneName);
 			});
-			*/
+
 			/*
 			// You must add this handler to avoid uncaught exceptions.
 			obs.on('error', err => {
