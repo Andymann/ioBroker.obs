1d9bee620dea6c3e57db8b57c4b9e2b9e31c7393
Andy Fischer <info@andyland.info>
2021-04-29 17:09:58 +0200

    make hostname and port accessible via admin interface

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const { throws } = require('node:assert');
 const OBSWebSocket = require('/../../../../home/pi/node_modules/obs-websocket-js');
 const obs = new OBSWebSocket();
 
