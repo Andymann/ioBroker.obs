6158109721af91431158441e4e7409cccdc5d6ea
Andy Fischer <info@andyland.info>
2021-05-03 11:04:26 +0200

    add states and listeners for volume changes

--- main.js
+++ main.js
@@ -239,7 +239,7 @@ class Obs extends utils.Adapter {
 							source: objSources[i]['name']
 						}).then(data => {
 							parentThis.log.info('createSourceListWithVolumeFader: getVolume auf ' + objSources[i]['name'] + '=' + data.volume);
-							let sDPName = 'obs.' + parentThis.instance + '.' + dpName;
+							let sDPName = 'obs.' + parentThis.instance + '.Volume.' + dpName;
 							parentThis.log.info(sDPName);
 							parentThis.setStateAsync(sDPName, data.volume, true);
 
