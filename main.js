8605692c98f9216729427bea0cd30481861145f5
Andy Fischer <info@andyland.info>
2021-05-03 07:50:45 +0200

    add states and listeners for volume changes

--- main.js
+++ main.js
@@ -169,6 +169,7 @@ class Obs extends utils.Adapter {
 		if (state) {
 			// The state was changed
 			//this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+			this.log.info('************ :' + state['name']);
 			parentThis.changeState(id, state.val, state.ack);
 		} else {
 			// The state was deleted
